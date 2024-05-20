9e16b17ca8bc23a52d41dc6fe9f55f2cc600eaa3
Agrim Nauriyal <agrim.nauriyal@gmail.com>
2024-05-20 11:30:31 +0530

    add button to choose grid size

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-const length = 16, breadth = 16;
+let gridSize = 16;
 
 const container = document.querySelector('.container');
 
-for(let i = 0; i < length; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-
-    for(let j = 0; j < breadth; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        row.appendChild(square);
+function buildGrid(gridSize) {
+    container.innerHTML = '';
+    for(let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+    
+        for(let j = 0; j < gridSize; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            row.appendChild(square);
+        }
+    
+        container.appendChild(row);
     }
-
-    container.appendChild(row);
 }
+
+buildGrid(gridSize);
 
 container.addEventListener('mouseover', (event) => {
     if(!event.target.classList.contains('square')) 
@@ -25,4 +30,23 @@ container.addEventListener('mouseout', (event) => {
     if(!event.target.classList.contains('square')) 
         return;
     event.target.style.backgroundColor = 'yellow';
+});
+
+const button = document.querySelector('button');
+console.log(button);
+const text = document.createElement('div');
+
+button.addEventListener('click', (event) => {
+    text.textContent = '';
+    let size = prompt("Enter the size of the grid: ");
+    size = Number(size);
+
+    if(isNaN(size)) {
+        text.textContent += 'Invalid Size!';
+        return;
+    }
+
+    if(size > 100)
+        size = 100;
+    buildGrid(size);
 });
